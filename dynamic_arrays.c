2b69f85b50b93540d071454d20c8dcd86ce81b0c
samyhaff <haffoudhisamy@gmail.com>
2022-05-06 21:21:31 +0200

    added functions

--- dynamic_arrays.c
+++ dynamic_arrays.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printarray(int *array, int length) {
-    for (int i = 0; i < length; i++)
-        printf("%d ", array[i]);
+typedef struct {
+    int length;
+    int max_length;
+    int* p;
+} dynamic_array;
+
+dynamic_array *create_array(int length) {
+    dynamic_array *arr = malloc(sizeof(dynamic_array));
+    arr->length = length;
+    arr->max_length = 2 * length;
+    arr->p = calloc(arr->max_length, sizeof(int));
+    return arr;
+}
+
+void  insert_element(dynamic_array *arr, int value) {
+    arr->length++;
+    if (arr->length > arr->max_length) {
+        arr->max_length *= 2;
+        arr->p = reallocarray(arr->p, arr->max_length, sizeof(int));
+    }
+    arr->p[arr->length-1] = value;
+}
+
+void printarray(dynamic_array *arr) {
+    for (int i = 0; i < arr->length; i++)
+        printf("%d ", arr->p[i]);
+    printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s length\n", argv[0]);
-        exit(1);
+    int length = 1;
+    dynamic_array *arr = create_array(length);
+    for (int i = 1; i <= 10; i++) {
+        insert_element(arr, i);
+        printarray(arr);
     }
-    int length = atoi(argv[1]);
-    int *p = malloc(sizeof(int) * length);
-    for (int i=0; i < length; i++)
-        p[i] = i;
-    printarray(p, length);
-    free(p);
+    free(arr->p);
+    free(arr);
     return 0;
 }
